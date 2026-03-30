0a500778af81612b1bb6894739bb11fb5d0b1900
abdelhafid37 <abdelhafidelhouari776@gmail.com>
2026-03-30 23:52:56 +0100

    fix: remove socket.io test listener

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,8 @@
 import { useState } from "react";
 import UsernameEntry from "./components/UsernameEntry";
-import socket from "./socket";
 
 function App() {
   const [username, setUsername] = useState(null);
-
-  socket.on();
 
   return username === null ? (
     <UsernameEntry setUsername={setUsername} />
